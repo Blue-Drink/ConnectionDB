cee974b7e325d693930858675060378812b6ac10
Blue <justme1random@gmail.com>
2026-02-10 10:10:24 +0100

    Create ItemsController

---
+++ Backend/Backend/Controllers/ItemsController.cs
@@ -0,0 +1,23 @@
+﻿using Backend.Models;
+using Backend.Models.Database;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ItemsController
+{
+    private readonly DataContext _dataContext;
+
+    public ItemsController(DataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+
+    [HttpGet]
+    public IEnumerable<Item> GetItems()
+    {
+        return _dataContext.Items;
+    }
+}
