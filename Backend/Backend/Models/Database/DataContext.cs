19f8d5ac2c1ff7ad1d75beec71bcefb983a59c05
Blue <justme1random@gmail.com>
2026-02-10 09:38:40 +0100

    Create datacontext

---
+++ Backend/Backend/Models/Database/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Models.Database;
+
+public class DataContext : DbContext
+{
+    public DbSet<Item> Items { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite($"DataSource=database.db");
+    }
+}
