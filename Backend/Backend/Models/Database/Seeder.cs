0ae7d43b1ea85e873acc2281551eddb0ab1265e7
Blue <justme1random@gmail.com>
2026-02-10 09:52:03 +0100

    Create initial 30 items

---
+++ Backend/Backend/Models/Database/Seeder.cs
@@ -0,0 +1,22 @@
+﻿namespace Backend.Models.Database;
+
+public class Seeder
+{
+    public void Seed(DataContext context)
+    {
+        if (!context.Items.Any())
+        {
+            for (int i = 1; i <= 30; i++)
+            {
+                context.Items.Add(new Item()
+                {
+                    Name = $"Artículo {i}",
+                    ImgUrl = $"https://picsum.photos/200?random={i}",
+                    Stock = new Random().Next(1, 100)
+                });
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
