9be00b205b829e5c144723b0521def30a0057cc7
Blue <justme1random@gmail.com>
2026-02-10 09:38:31 +0100

    Create Item model

---
+++ Backend/Backend/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend.Models;
+
+public class Item
+{
+    public int Id { get; set; }
+
+    public required string Name { get; set; }
+
+    public required string ImgUrl { get; set; }
+
+    public required int Stock { get; set; }
+}
