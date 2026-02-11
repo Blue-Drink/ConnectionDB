002ca977c0f00f5defdf8ed79fc2d250a15ff2a7
Blue <justme1random@gmail.com>
2026-02-11 21:27:29 +0100

    Implement base angular structure and item service

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,12 +1,32 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from "@angular/common";
+import { Component, inject, OnInit } from "@angular/core";
+import { ItemService } from "./services/item.service";
+import { Item } from "./models/item.model";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('Frontend');
+
+export class AppComponent implements OnInit {
+  title = 'Frontend';
+
+  private itemService = inject(ItemService);
+
+  public items: Item[] = [];
+
+  ngOnInit(): void {
+    this.itemService.getItems().subscribe({
+      next: (data) => {
+        this.items = data;
+        console.log('Datos recibidos:', this.items);
+      },
+      error: (err) => {
+        console.error('Error la conectar con la API:', err);
+      }
+    })
+  }
 }
---
+++ Frontend/src/app/services/item.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Item } from "../models/item.model";
+
+@Injectable({
+     providedIn: 'root'
+})
+
+export class ItemService {
+     private apiUrl = 'https://localhost:7221/api/items';
+
+     constructor(private http: HttpClient){}
+
+     getItems(): Observable<Item[]> {
+          return this.http.get<Item[]>(this.apiUrl);
+     }
+}
