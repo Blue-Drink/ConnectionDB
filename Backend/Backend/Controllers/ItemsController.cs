6c68aaae1245157be3aa4753c58c8928937efc66
Blue <justme1random@gmail.com>
2026-02-11 19:46:02 +0100

    Searcher that filters and sorts, and that ignores caps and tildas

--- Backend/Backend/Controllers/ItemsController.cs
+++ Backend/Backend/Controllers/ItemsController.cs
@@ -1,12 +1,13 @@
 ﻿using Backend.Models;
 using Backend.Models.Database;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class ItemsController
+public class ItemsController : ControllerBase
 {
     private readonly DataContext _dataContext;
 
@@ -16,8 +17,37 @@ public class ItemsController
     }
 
     [HttpGet]
-    public IEnumerable<Item> GetItems()
+    public async Task<ActionResult<IEnumerable<Item>>> GetItems([FromQuery] ItemQuery queryParams)
     {
-        return _dataContext.Items;
+        var items = await _dataContext.Items.ToListAsync();
+        var result = items.AsEnumerable();
+
+        if (!string.IsNullOrEmpty(queryParams.Search))
+        {
+            string searchNormalized = RemoveAccents(queryParams.Search.ToLower());
+
+            result = result.Where(i => RemoveAccents(i.Name.ToLower()).Contains(searchNormalized));
+        }
+
+        if (queryParams.Sort!.ToLower() == "desc")
+        {
+            result = result.OrderByDescending(i => i.Name);
+        }
+        else
+        {
+            result = result.OrderBy(i => i.Name);
+        }
+
+        return Ok(result.ToList());
+    }
+
+    private string RemoveAccents(string text)
+    {
+        if (string.IsNullOrWhiteSpace(text)) return text;
+
+        return new string(text
+            .Normalize(System.Text.NormalizationForm.FormD)
+            .Where(ch => System.Globalization.CharUnicodeInfo.GetUnicodeCategory(ch) != System.Globalization.UnicodeCategory.NonSpacingMark)
+            .ToArray());
     }
 }
---
+++ Backend/Backend/Models/ItemQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace Backend.Models;
+
+public class ItemQuery
+{
+    public string? Search { get; set; }
+    public string? Sort { get; set; }
+}
