b027b50cbef852161383d81b4d16a27497798665
Blue <justme1random@gmail.com>
2026-02-11 19:19:55 +0100

    Usar Swagger

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -10,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<DataContext>();
 
 var app = builder.Build();
@@ -18,6 +20,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
     app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 }
