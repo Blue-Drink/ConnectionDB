d87e21339f7b1da593eebbc0f346ae6f30eb6c75
Blue <justme1random@gmail.com>
2026-02-16 10:50:48 +0100

    update item.service for error administration

--- Frontend/src/app/services/item.service.ts
+++ Frontend/src/app/services/item.service.ts
@@ -2,29 +2,50 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Item } from "../models/item.model";
+import { catchError, throwError } from "rxjs";
 
 @Injectable({
-     providedIn: 'root'
+	providedIn: 'root'
 })
 
 export class ItemService {
-     private apiUrl = 'https://localhost:7221/api/items';
+	private apiUrl = 'https://localhost:7221/api/items';
 
-     constructor(private http: HttpClient){}
+	constructor(private http: HttpClient){}
 
-     getItems(): Observable<Item[]> {
-          return this.http.get<Item[]>(this.apiUrl);
-     }
+	getItems(): Observable<Item[]> {
+		return this.http.get<Item[]>(this.apiUrl).pipe(
+			catchError(error => {
+				console.error('Error en la petición:', error);
+				return throwError(() => new Error('No se pudo obtener la lista de artículos.'))
+			})
+		);
+	}
 
-     postItems(item: Item): Observable<Item> {
-          return this.http.post<Item>(this.apiUrl, item);
-     }
+	postItems(item: Item): Observable<Item> {
+		return this.http.post<Item>(this.apiUrl, item).pipe(
+			catchError(error => {
+				console.error('Error en la petición', error);
+				return throwError(() => new Error('No se pudo añadir el artículo.'))
+			})
+		);
+	}
 
-     updateItem(id: number, item: Item): Observable<any> {
-          return this.http.put(`${this.apiUrl}/${id}`, item);
-     }
+	updateItem(id: number, item: Item): Observable<any> {
+		return this.http.put(`${this.apiUrl}/${id}`, item).pipe(
+			catchError(error => {
+				console.error('Error en la petición', error);
+				return throwError(() => new Error('No se pudo modificar el artículo.'))
+			})
+		);
+	}
 
-     deleteItem(id: number): Observable<any> {
-          return this.http.delete(`${this.apiUrl}/${id}`);
-     }
+	deleteItem(id: number): Observable<any> {
+		return this.http.delete(`${this.apiUrl}/${id}`).pipe(
+			catchError(error => {
+				console.error('Error en la petición', error);
+				return throwError(() => new Error('No se pudo eliminar el artículo.'))
+			})
+		);
+	}
 }
