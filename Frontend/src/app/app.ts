8669b2ceba8aa1f46d0c8ce717424b6f698f0505
Blue <justme1random@gmail.com>
2026-02-16 10:42:17 +0100

    implemented search

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -5,90 +5,91 @@ import { Item } from "./models/item.model";
 import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, FormsModule],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+	selector: 'app-root',
+	standalone: true,
+	imports: [CommonModule, FormsModule],
+	templateUrl: './app.html',
+	styleUrl: './app.css'
 })
 
 export class AppComponent implements OnInit {
-  title = 'Frontend';
+public items: Item[] = [];
+public filteredItems: Item[] = [];
+public searchTerm: string = '';
 
-  private itemService = inject(ItemService);
+ngOnInit(): void {
+	this.itemService.getItems().subscribe({
+		next: (data) => {
+			this.items = data;
+			this.filteredItems = data;
+		},
+		error: (err) => {
+			console.error('Error la conectar con la API:', err);
+		}
+	})
+}
 
-  public items: Item[] = [];
+filterItems() {
+	const term = this.searchTerm.toLowerCase();
+	this.filteredItems = this.items.filter(item =>
+		item.name.toLowerCase().includes(term)
+	)
+}
 
-  ngOnInit(): void {
-    this.itemService.getItems().subscribe({
-      next: (data) => {
-        this.items = data;
-        console.log('Datos recibidos:', this.items);
-      },
-      error: (err) => {
-        console.error('Error la conectar con la API:', err);
-      }
-    })
-  }
+title = 'Frontend';
 
-  showModal: boolean = false;
+private itemService = inject(ItemService);  
 
-  newItem: Item = {
-    id: 0,
-    name: '',
-    imgUrl: '',
-    stock: 0
-  };
+showModal: boolean = false;
 
-  saveItem() {
-    if (this.isEditing) {
-      this.itemService.updateItem(this.newItem.id, this.newItem).subscribe({
-        next: () => {
-          const index = this.items.findIndex(i => i.id === this.newItem.id);
-          this.items[index] = {...this.newItem};
-          this.closeModal();
-        },
-        error: (err) => console.error(err)
-      });
-    } else {
-      this.itemService.postItems(this.newItem).subscribe({
-        next: (res) => {
-          this.items.push(res);
-          this.closeModal();
-          console.log('Guardado con éxito');
-        },
-        error: (err) => console.error('Error al guardar:', err)
-      });
-    }
-  }
+newItem: Item = { id: 0, name: '', imgUrl: '', stock: 0 };
 
-  openModal() { this.showModal = true; }
-  closeModal() {
-    this.showModal = false;
-    this.isEditing = false;
-    this.newItem = {
-      id: 0,
-      name: '',
-      imgUrl: '',
-      stock: 0
-    }
-  }
+saveItem() {
+	if (this.isEditing) {
+		this.itemService.updateItem(this.newItem.id, this.newItem).subscribe({
+			next: () => {
+				const index = this.items.findIndex(i => i.id === this.newItem.id);
+				this.items[index] = {...this.newItem};
+				this.closeModal();
+			},
+			error: (err) => console.error(err)
+		});
+	} else {
+		this.itemService.postItems(this.newItem).subscribe({
+			next: (res) => {
+				this.items.push(res);
+				this.filterItems();
+				this.closeModal();
+				console.log('Guardado con éxito');
+			},
+			error: (err) => console.error('Error al guardar:', err)
+		});
+	}
+}
 
-  isEditing: boolean = false;
+openModal() { this.showModal = true; }
+closeModal() {
+	this.showModal = false;
+	this.isEditing = false;
+	this.newItem = { id: 0, name: '', imgUrl: '', stock: 0 }
+}
 
-  openEditModal(item: Item) {
-    this.isEditing = true;
-    this.showModal = true;
-    this.newItem = {...item};
-  }
+isEditing: boolean = false;
 
-  deleteItem(id: number) {
-    if (confirm('¿Está seguro de querer eliminar este artículo?')) {
-      this.itemService.deleteItem(id).subscribe({
-        next: () => {
-          this.items = this.items.filter(i => i.id !== id);
-        }
-      })
-    }
-  }
+openEditModal(item: Item) {
+	this.isEditing = true;
+	this.showModal = true;
+	this.newItem = {...item};
+}
+
+deleteItem(id: number) {
+	if (confirm('¿Está seguro de querer eliminar este artículo?')) {
+		this.itemService.deleteItem(id).subscribe({
+		next: () => {
+			this.items = this.items.filter(i => i.id !== id);
+			this.filterItems();
+		}
+		})
+	}
+	}
 }
